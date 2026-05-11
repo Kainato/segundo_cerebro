a12269ba1da2a9c1ce87aab81458c4f0856c1348
Kainato <caiocaladaraujo@gmail.com>
2026-05-11 15:28:36 -0300

    Adiciona regex para ativar a navegação na página inicial

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -85,6 +85,7 @@ const config: Config = {
         to: '/',
         position: 'left',
         label: 'Página Inicial',
+        activeBaseRegex: '^/$',
       },
       {
         type: 'docSidebar',
